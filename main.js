68697e234dc70d527541f2e2579531a7ff2ac9d6
VladKurluk <vlad11kurluk89@gmail.com>
2018-03-14 10:27:49 +0200

    Окончательный вариант калькулятора

--- main.js
+++ main.js
@@ -1,28 +1,46 @@
 $(document).ready(function(){
+    /*Подвешиваем на событие(изменение селекта) и записываем в переменные значения опшинов*/
     $('.calc select').change(function(){
+        //Получаем значения data-атрибутов
         $edition = $('select#card_edition').val();
-        console.log($edition);
         $paper = $('select#card_paper').val();
-        console.log($paper);
+        $paper_ratio = $('select#card_paper option:selected').attr('data-paper-ratio');
         $color = $('select#card_color').val();
-        console.log($color);
-        $color_variant = $('#card_color > option').attr('data-color');
-        console.log($color_variant);
+        $print_file = $('select#card_color option:selected').attr('data-print-file');
+        $print = $('select#card_color option:selected').attr('data-print');
+
+        /*Логика калькулятора*/
+        $division = 30; //Тиражирование
+        $final_edition = $edition / $division;
+        $price = $final_edition * $paper_ratio + parseInt($print_file) +  $final_edition * $print;
+        $('#final_price').text($price);//Вывод цены
+    });
+
+
+    $('select#card_color').change(function(){
+        /*Получаем значение data-атрибута и меняем в зависимости от этого картинку выбранной визитки*/
+        $color_variant = $('select#card_color option:selected').attr('data-number');
         if ($color_variant == 1) {
             $('.card-images img').hide();
             $('#card_1_0').show('slow');
-        } else if ($color_variant == 2) {
+        }
+        if ($color_variant == 2) {
             $('.card-images img').hide();
             $('#card_1_1').show('slow');
-        } else if ($color_variant == 3) {
+        }
+        if ($color_variant == 3) {
             $('.card-images img').hide();
             $('#card_4_0').show('slow');
-        } else if ($color_variant == 4) {
+        }
+        if ($color_variant == 4) {
             $('.card-images img').hide();
             $('#card_4_1').show('slow');
-        } else if ($color_variant == 4) {
+        }
+        if ($color_variant == 5) {
             $('.card-images img').hide();
             $('#card_4_4').show('slow');
         }
-    })
+    });
+
+    
 });
